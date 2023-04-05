979ddb76a05bd564643878f1c35d3e3744450ae2
Nicholas Pritchyk <nickpritchyk20@gmail.com>
2023-04-04 21:31:04 -0600

    commit 2

--- main.py
+++ main.py
@@ -4,26 +4,20 @@ import numpy as np
 import random
 import pylab
 
+
 time_steps = 100000
+regions = [TropicalAfrica, UK, Alaska, SouthAmerica, Oceania, EastAsia]
 temperature = []
-greenhouse_gases_ranges = []
-location = []
-species = ["Pied Wheatear", "Northern Wheater", "Pectoral Sandpiper", "Bar-tailed Godwit", "Short-tailed Shearwater"]
+species = ["Pied Wheatear", "Northern Wheatear", "Pectoral Sandpiper", "Bar-tailed Godwit", "Short-tailed Shearwater"]
 
 
 def migrate():
     print('Starting Migration...')
 
-    for i in range(0, location.length()):
+    for i in range(0, 43):
+        for j in range(0, species.length()):
 
-        for i in range(0, time_steps):
-            for j in range(0, species.length()):
-                if(i == location[0]):
-                    print("First Migration Path")
-                if(i == location[1]):
-                    print("Second Migration Path")
-                if(i == location[2]):
-                    print("Third Migration Path")
+
 
 
 migrate()
